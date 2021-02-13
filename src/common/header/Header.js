1163a16ef068062ace9276a6a1c225361cdce013
subhankarp <subhankarp@juniper.net>
2021-02-14 03:48:52 +0530

    Added modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,16 +3,34 @@ import React, {Component} from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
+import Modal from 'react-modal';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalIsOpen: false
+        };
+    }
+    openModalHandler = () => {
+        this.setState({modalIsOpen: true});
+    }
+    closeModalHandler = () => {
+        this.setState({modalIsOpen: false});
+    }
     render() {
         return (
-            <div className="header-container">
-                <img src={logo} className="app-logo" alt="logo" />
-                <Button variant="contained" color="default">
-                    Login
-                </Button>
-            </div>
+            <React.Fragment>
+                <header className="header-container">
+                    <img src={logo} className="app-logo" alt="logo" />
+                    <Button variant="contained" color="default" onClick={this.openModalHandler}>
+                        Login
+                    </Button>
+                </header>
+                <Modal ariaHideApp={false} onRequestClose={this.closeModalHandler} isOpen={this.state.modalIsOpen} contentLabel="Login">
+
+                </Modal>
+            </React.Fragment>
         )
     }
 }
