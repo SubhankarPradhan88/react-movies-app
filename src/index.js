a25903f3e2c89640f40a9d187d758f5423f904cf
subhankarp <subhankarp@juniper.net>
2021-02-13 23:35:44 +0530

    Added ‘Login’ button on Header

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div></div>
+    <Home/>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import './Home.css';
+import Header from '../../common/header/Header';
 
 class Home extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <Header />
+            </div>
         )
     }
 }
