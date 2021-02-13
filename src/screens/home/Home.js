0a4c95c091f0bddfc5daa577d0ac5e8ef5dbfc16
subhankarp <subhankarp@juniper.net>
2021-02-13 19:46:21 +0530

    Added ‘Home’ component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Home;
